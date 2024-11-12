8a9945a1ead7a9f8dfdec79a272ff6326722f37c
Sir-Leon1 <eb3.61594.22@student.chuka.ac.ke>
2024-11-12 16:45:44 +0300

    Modified methods in auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,6 +5,7 @@ Module of Auth views
 """
 
 from typing import List, TypeVar
+
 from flask import request
 
 
@@ -22,7 +23,21 @@ class Auth:
         Returns:
             bool: True if the path requires authentication, False otherwise.
         """
-        return False
+        if path is None:
+            return True
+        elif excluded_paths is None or excluded_paths == []:
+            return True
+        elif path in excluded_paths:
+            return False
+        else:
+            for i in excluded_paths:
+                if i.startswith(path):
+                    return False
+                if path.startswith(i):
+                    return False
+                if i[-1] == '*' and path.startswith(i[:-1]):
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
@@ -32,7 +47,9 @@ class Auth:
         Returns:
             str: The authorization header if present, None otherwise.
         """
-        return None
+        if request is None or "Authorization" not in request.headers:
+            return None
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar("User"):
         """
