cfc7a25f07c5b963176f4f73e5d5607da45c94a9
Sir-Leon1 <eb3.61594.22@student.chuka.ac.ke>
2024-11-12 17:10:46 +0300

    Updated the auth method in auth, updated app.py with before request

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,31 +4,55 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
+from api.v1.auth.auth import Auth
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
+from flask_cors import CORS, cross_origin
 import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+if os.getenv("AUTH_TYPE") == "auth":
+    auth = Auth()
+
+
+@app.before_request
+def before_request():
+    """Before request:
+    to filter each request b4 its handled
+    """
+    if auth is None:
+        return
+    else:
+        excluded = [
+            "/api/v1/status/",
+            "/api/v1/unauthorized/",
+            "/api/v1/forbidden/"
+        ]
+        if auth.require_auth(request.path, excluded):
+            if auth.authorization_header(request) is None:
+                abort(401, description="Unauthorized")
+            if auth.current_user(request) is None:
+                abort(403, decription="Forbidden")
 
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
-    """
+    """Not found handler"""
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Unauthorized handler"""
+    """Unauthorized handler"""
     return jsonify({"error": "Unautorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """ Forbidden handler"""
+    """Forbidden handler"""
     return jsonify({"error": "Forbidden"}), 403
 
 
