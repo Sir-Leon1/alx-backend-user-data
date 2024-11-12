ffa6db6dd25ee77271416197de5a9451b6c59da1
Sir-Leon1 <eb3.61594.22@student.chuka.ac.ke>
2024-11-12 16:15:02 +0300

    Forbidden endpoint

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,6 +25,7 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+
 @app_views.route('/unauthorized', strict_slashes=False)
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
@@ -32,3 +33,12 @@ def unauthorized() -> str:
         - Unauthorized error
     """
     abort(401, description="Unauthorized");
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+        - Forbidden error
+    """
+    abort(403, description="Forbidden");
